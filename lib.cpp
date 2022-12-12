fb9d7a444ba06d4c3f5724addb7df302075c0080
RicciardiRiccardo <117997389+RicciardiRiccardo@users.noreply.github.com>
2022-12-12 22:18:06 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
 
+#include "lib.h"
+
+float arr(float m[],int n){
+    float a = m[0];
+
+    for(int i=1;i<n;i++){
+        if(m[i] > a){
+            a = m[i];
+        }
+    }
+    return a;
+} 
